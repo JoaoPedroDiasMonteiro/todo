83c9e4e81bcecf820d90ce1b2f254412a38c8135
João Pedro <joaopedro453@hotmail.com>
2021-05-30 00:30:09 -0300

    add metodo index de TodoController

--- app/TodoController.php
+++ app/TodoController.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoCreateRequest;
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class TodoController extends Controller
 {
@@ -15,18 +18,11 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Todo/Index', [
+            'todos' => Todo::withRecentCompleted()->get()
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,7 +30,7 @@ class TodoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TodoCreateRequest $request)
     {
         //
     }
